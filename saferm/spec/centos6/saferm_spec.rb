9f364e31b2a544cd7dfc73c8066702135ade9105
Gustavo Hoyer <gustavo.hoyer@gmail.com>
2016-06-16 23:44:35 -0300

    Adds serverspec  files to test saferm

---
+++ saferm/spec/centos6/saferm_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe file('/etc/safe-rm.conf') do
+  it {
+    should be_file
+    should exist
+  }
+  its(:content) { should match('/dev') }
+end
+
+describe file('/usr/bin/safe-rm') do
+  it{
+    should be_file
+    should exist
+  }
+end
+describe command('/usr/bin/safe-rm /bin') do
+  its(:stderr) { should match('skipping') }
+end
